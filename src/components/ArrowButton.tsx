9d92b8c97c1d981bd7a02a6a3f07309e8e5cec52
EdSabhin <edsabhin@yahoo.com>
2024-02-21 22:29:05 -0500

    Improved ArrowButtons.

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -10,7 +10,7 @@ const ArrowButton = ({ direction }:ArrowButtonProps) => {
       <button className='w-[3.2rem] h-[3.2rem] flex justify-center 
          items-center text-2xl font-black
        hover:text-rose-700 rounded-full
-       bg-rose-950 hover:bg-white opacity-70 hover:opacity-90
+       bg-gradient-to-br hover:from-rose-950 hover:to-slate-300 bg-white opacity-70 hover:opacity-90
          transition duration-500 '>
       {direction}
       </button>
