d58e898e12fb9a92678430cc38c8317e3cbfee53
EdSabhin <edsabhin@yahoo.com>
2024-02-22 20:45:43 -0500

    Added map() to render cards per project.

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SliderCard from "../components/SliderCard"
 import ArrowButton from './ArrowButton'
 
@@ -8,8 +8,8 @@ const Slider = () => {
   return (
     <div className="w-[85%] flex">
       <div className='w-1/7 flex items-center pl-24 mr-32 gap-7'>
-        <ArrowButton direction="&lt;" />
-        <ArrowButton direction="&gt;" />
+        <ArrowButton direction="&lt;" onClick={()=>""}/>
+        <ArrowButton direction="&gt;" onClick={()=>""}/>
       </div>
       <div className="flex gap-8">
         {Object.values(projects).map((project, index) => (
@@ -21,7 +21,6 @@ const Slider = () => {
         />
       ))}
       </div>
-      
     </div>
   )
 }
--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,28 +1,28 @@
-interface Project {
+interface Projects {
   image: string;
   title: string;
   description: string;
 }
 
-export const projects: Record<string, Project> = {
-  projectOne: {
+export const projects: Projects[] = [
+  {
     image: "/assets/images/img1.jpg",
     title: "PROJECT 1",
     description: "Description of Project 1",
   },
-  projectTwo: {
+  {
     image: "/assets/images/img2.jpg",
     title: "PROJECT 2",
     description: "Description of Project 2",
   },
-  projectThree: {
+  {
     image: "/assets/images/img3.jpg",
     title: "PROJECT 3",
     description: "Description of Project 3",
   },
-  projectFour: {
+  {
     image: "/assets/images/img4.jpg",
     title: "PROJECT 4",
     description: "Description of Project 4",
   },
-};
+];
