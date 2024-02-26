dc09adadb1fa332afd27e0319c4f7b3263f05d83
EdSabhin <edsabhin@yahoo.com>
2024-02-26 12:59:22 -0500

    Finished basic styling and responsiveness.

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -9,7 +9,7 @@ const ArrowButton = ({ id, direction, 'aria-label': ariaLabel }) => {
   return (
     <>
       <button id={id} aria-label={ariaLabel}
-        className='w-[2.7rem] h-[2.7rem] lg:w-[3.2rem] lg:h-[3.2rem] flex justify-center items-center pb-[0.2rem] text-xl lg:text-2xl font-base hover:border-stone-100 bg-stone-100 opacity-40 hover:opacity-90 transition duration-500 rounded-full'
+        className='w-[2.7rem] h-[2.7rem] lg:w-[3.2rem] lg:h-[3.2rem] flex justify-center items-center pb-[0.2rem] text-xl lg:text-2xl font-base hover:border-stone-100 bg-stone-100 opacity-40 hover:opacity-90 transition duration-300 rounded-full'
       >{direction}
       </button>
     </>
--- src/components/SliderCard.tsx
+++ src/components/SliderCard.tsx
@@ -8,7 +8,7 @@ interface sliderCardProps {
 
 const SliderCard = ({image, title, description}:sliderCardProps) => {
   return (
-    <div className='item w-40 h-64 relative rounded-lg cursor-pointer'>
+    <div className='item w-40 h-64 relative rounded-lg'>
       <img src={image} className='w-60 h-64 object-cover rounded-tl-3xl rounded-br-3xl rounded' />
       <div className="absolute bottom-0 px-4 pb-4">
         <h2 className="title text-base cormorant-infant-medium-italic 
