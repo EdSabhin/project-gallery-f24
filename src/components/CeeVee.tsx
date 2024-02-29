a79a73c8e65bb582c4028a02acb2ced59efadbcc
EdSabhin <edsabhin@yahoo.com>
2024-02-28 19:09:21 -0500

    Added spin animation to CV icon.

--- src/components/CeeVee.tsx
+++ src/components/CeeVee.tsx
@@ -10,7 +10,7 @@ const CeeVee = () => {
       {/* Title */}
       <div>
         <div className="w-full flex flex-col lg:flex-row justify-center lg:justify-start items-center">
-          <img src="/assets/images/cv-icon.svg" alt="" className="w-2/3 lg:w-1/3 mb-8 lg:mr-24" />
+          <img src="/assets/images/cv-icon.svg" alt="" className="w-2/3 lg:w-1/3 mb-8 lg:mr-24 animate-[spin_25s_linear_infinite]"/>
           <div className="flex flex-col pt-4 pb-16">
               <h1 className="kode-mono-light text-3xl pb-6">Eduardo Sabogal</h1>
               <h2 className="gradient-text-cv kode-mono-bold text-4xl mb-12">Front-end Developer</h2>
