9999cd2c1385fbd1cc4945219b39f79654fde03d
EdSabhin <edsabhin@yahoo.com>
2024-06-04 18:32:30 -0500

    Re-added videos for SliderCards.

---
+++ src/components/LoadingGallery.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LoadingGallery = () => {
+;
+  return (
+    <div className="loading-element moving-gradient">
+      <h1 className="cormorant-sc-regular text-3xl text-rose-400">
+            Loading Gallery...
+      </h1>
+    </div>
+  );
+};
+
+export default LoadingGallery;
+
