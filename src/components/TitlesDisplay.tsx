f1606bfb2a92dd888eb8654e9cd1025223f83be9
EdSabhin <edsabhin@yahoo.com>
2024-06-06 18:56:58 -0500

    Typo fix

--- src/components/TitlesDisplay.tsx
+++ src/components/TitlesDisplay.tsx
@@ -19,7 +19,7 @@ const TitlesDisplay = () => {
             <h2 className="title pb-4 text-3xl lg:text-5xl cormorant-sc-semibold text-stone-100">{project.title}</h2>
             <h2 className="topic pb-2 text-4xl gradient-text cormorant-infant-medium-italic">{project.projectTitle.toUpperCase()}</h2>
             <p className="itemDescription text-xl lg:text-2xl cormorant-infant-regular text-stone-100">{project.description}</p>
-            <div className="buttons flex flex-col  -mmt-14 gap-10">
+            <div className="buttons flex flex-col mt-14 gap-10">
               <div className="flex gap-12 lg:gap-12">
                 <LinkButton href={project.href} text="Visit" className="" />
                 <LinkButton href="/cv" text="View CV" className="desktop-only" />
