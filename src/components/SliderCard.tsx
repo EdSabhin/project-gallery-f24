12b6165e8943c3545986baa6b58d2861247e4d40
EdSabhin <edsabhin@yahoo.com>
2024-06-08 15:19:44 -0500

    Fixed indentation formatting issues.

--- src/components/SliderCard.tsx
+++ src/components/SliderCard.tsx
@@ -14,17 +14,16 @@ const SliderCard = ({video, title, description, shortDescription, href}:sliderCa
     <div className='item relative'>
       <div className='h-[4rem]'>
         <CardButton href={href} text='View Project' className="absolute rounded-tl-3xl rounded z-10"/>  
-      <video autoPlay loop muted playsInline className='w-full h-full object-cover absolute top-0 rounded-tl-3xl rounded-br-3xl rounded ' >
-        <source src={video} type="video/mp4" />
+        <video autoPlay loop muted playsInline className='w-full h-full object-cover absolute top-0 rounded-tl-3xl rounded-br-3xl rounded ' >
+          <source src={video} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
-      <div className="w-full absolute bottom-0 py-2 px-4 bg-stone-800 bg-opacity-85 rounded-br-3xl rounded-bl-sm moving-card-gradient">
-        <h2 className="title text-base cormorant-infant-semibold-italic 
+        </video>
+        <div className="w-full absolute bottom-0 py-2 px-4 bg-stone-800 bg-opacity-85 rounded-br-3xl rounded-bl-sm moving-card-gradient">
+          <h2 className="title text-base cormorant-infant-semibold-italic 
             text-rose-500">{title.toUpperCase()}</h2>
-         <p className="text-stone-100 text-base cormorant-infant-regular">{shortDescription}</p>
-      </div>
-      </div>
-      
+           <p className="text-stone-100 text-base cormorant-infant-regular">{shortDescription}</p>
+        </div>
+      </div> 
     </div>
   )
 }
